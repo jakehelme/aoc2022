bce5c284ddc1e2dae913f509e5220eaff0c5d587
Jake Helme <jake.helme@gmail.com>
2022-12-09 23:51:32 +0200

    d9

--- d9.js
+++ d9.js
@@ -22,24 +22,28 @@ const dWidth = moves
 const gridWidth = rWidth + lWidth + 2;
 const gridHeight = uWidth + dWidth + 2;
 
-const grid = [];
-
-for (let y = 0; y < gridHeight; y++) {
-	grid.push([]);
-	for (let x = 0; x < gridWidth; x++) {
-		grid[y].push('.');
+function generateGrid() {
+	const grid = [];
+	for (let y = 0; y < gridHeight; y++) {
+		grid.push([]);
+		for (let x = 0; x < gridWidth; x++) {
+			grid[y].push('.');
+		}
 	}
+	return grid;
 }
 
-const headPos = [Math.ceil(gridHeight / 2), Math.ceil(gridWidth / 2)];
+let grid = generateGrid();
+
+let headPos = [Math.ceil(gridHeight / 2), Math.ceil(gridWidth / 2)];
 const tailPos = [Math.ceil(gridHeight / 2), Math.ceil(gridWidth / 2)];
 
-function tailIsAdjacent() {
+function knotsAreAdjacent(leadKnot, trailingKnot) {
 	return (
-		tailPos[0] >= headPos[0] - 1 &&
-		tailPos[0] <= headPos[0] + 1 &&
-		tailPos[1] >= headPos[1] - 1 &&
-		tailPos[1] <= headPos[1] + 1
+		trailingKnot[0] >= leadKnot[0] - 1 &&
+		trailingKnot[0] <= leadKnot[0] + 1 &&
+		trailingKnot[1] >= leadKnot[1] - 1 &&
+		trailingKnot[1] <= leadKnot[1] + 1
 	);
 }
 
@@ -49,28 +53,28 @@ for (const move of moves) {
 		switch (dir) {
 			case 'R':
 				headPos[1] = headPos[1] + 1;
-				if (!tailIsAdjacent()) {
+				if (!knotsAreAdjacent(headPos, tailPos)) {
 					tailPos[1] = tailPos[1] + 1;
 					if (headPos[0] !== tailPos[0]) tailPos[0] = headPos[0];
 				}
 				break;
 			case 'D':
 				headPos[0] = headPos[0] + 1;
-				if (!tailIsAdjacent()) {
+				if (!knotsAreAdjacent(headPos, tailPos)) {
 					tailPos[0] = tailPos[0] + 1;
 					if (headPos[1] !== tailPos[1]) tailPos[1] = headPos[1];
 				}
 				break;
 			case 'L':
 				headPos[1] = headPos[1] - 1;
-				if (!tailIsAdjacent()) {
+				if (!knotsAreAdjacent(headPos, tailPos)) {
 					tailPos[1] = tailPos[1] - 1;
 					if (headPos[0] !== tailPos[0]) tailPos[0] = headPos[0];
 				}
 				break;
 			case 'U':
 				headPos[0] = headPos[0] - 1;
-				if (!tailIsAdjacent()) {
+				if (!knotsAreAdjacent(headPos, tailPos)) {
 					tailPos[0] = tailPos[0] - 1;
 					if (headPos[1] !== tailPos[1]) tailPos[1] = headPos[1];
 				}
@@ -82,6 +86,110 @@ for (const move of moves) {
 	}
 }
 
-const result = grid.map(x => x.join('')).join('').split('').filter(x => x === '#').length;
+console.log(
+	grid
+		.map((x) => x.join(''))
+		.join('')
+		.split('')
+		.filter((x) => x === '#').length
+);
 
-console.log(result);
+grid = generateGrid();
+
+const knots = [];
+for (let i = 0; i < 10; i++) {
+	knots.push([Math.ceil(gridHeight / 2), Math.ceil(gridWidth / 2)]);
+}
+
+for (const move of moves) {
+	const [dir, dist] = move;
+	for (let i = 0; i < dist; i++) {
+		switch (dir) {
+			case 'R':
+				knots[0][1] = knots[0][1] + 1;
+				for (let i = 1; i < knots.length; i++) {
+					if (!knotsAreAdjacent(knots[i - 1], knots[i])) {
+						if (knots[i - 1][1] > knots[i][1]) {
+							knots[i][1] = knots[i][1] + 1;
+						} else if (knots[i - 1][1] < knots[i][1]) {
+							knots[i][1] = knots[i][1] - 1;
+						}
+
+						if (knots[i - 1][0] > knots[i][0]) {
+							knots[i][0] = knots[i][0] + 1;
+						} else if (knots[i - 1][0] < knots[i][0]) {
+							knots[i][0] = knots[i][0] - 1;
+						}
+					}
+				}
+
+				break;
+			case 'D':
+				knots[0][0] = knots[0][0] + 1;
+				for (let i = 1; i < knots.length; i++) {
+					if (!knotsAreAdjacent(knots[i - 1], knots[i])) {
+						if (knots[i - 1][0] > knots[i][0]) {
+							knots[i][0] = knots[i][0] + 1;
+						} else if (knots[i - 1][0] < knots[i][0]) {
+							knots[i][0] = knots[i][0] - 1;
+						}
+
+						if (knots[i - 1][1] > knots[i][1]) {
+							knots[i][1] = knots[i][1] + 1;
+						} else if (knots[i - 1][1] < knots[i][1]) {
+							knots[i][1] = knots[i][1] - 1;
+						}
+					}
+				}
+				break;
+			case 'L':
+				knots[0][1] = knots[0][1] - 1;
+				for (let i = 1; i < knots.length; i++) {
+					if (!knotsAreAdjacent(knots[i - 1], knots[i])) {
+						if (knots[i - 1][1] > knots[i][1]) {
+							knots[i][1] = knots[i][1] + 1;
+						} else if (knots[i - 1][1] < knots[i][1]) {
+							knots[i][1] = knots[i][1] - 1;
+						}
+
+						if (knots[i - 1][0] > knots[i][0]) {
+							knots[i][0] = knots[i][0] + 1;
+						} else if (knots[i - 1][0] < knots[i][0]) {
+							knots[i][0] = knots[i][0] - 1;
+						}
+					}
+				}
+				break;
+			case 'U':
+				knots[0][0] = knots[0][0] - 1;
+				for (let i = 1; i < knots.length; i++) {
+					if (!knotsAreAdjacent(knots[i - 1], knots[i])) {
+						if (knots[i - 1][0] > knots[i][0]) {
+							knots[i][0] = knots[i][0] + 1;
+						} else if (knots[i - 1][0] < knots[i][0]) {
+							knots[i][0] = knots[i][0] - 1;
+						}
+
+						if (knots[i - 1][1] > knots[i][1]) {
+							knots[i][1] = knots[i][1] + 1;
+						} else if (knots[i - 1][1] < knots[i][1]) {
+							knots[i][1] = knots[i][1] - 1;
+						}
+					}
+				}
+				break;
+			default:
+				throw new Error('nope');
+		}
+
+		grid[knots[9][0]][knots[9][1]] = '#';
+	}
+}
+
+console.log(
+	grid
+		.map((x) => x.join(''))
+		.join('')
+		.split('')
+		.filter((x) => x === '#').length
+);
